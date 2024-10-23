2bbafcd2775e1020c4be280d2a63c084222233f3
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-10-24 00:27:56 +0200

    appbar creata

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -17,22 +16,22 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Vynt'),
+      home: const Feed(title: 'Vynt'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Feed extends StatefulWidget {
+  const Feed({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Feed> createState() => _FeedState();
 }
 
 enum _SelectedTab { home, search, add, library, profile }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FeedState extends State<Feed> {
   _SelectedTab _selectedTab = _SelectedTab.home;
 
   void _handleIndexChanged(int index) {
@@ -45,61 +44,99 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: _buildBody(),
+      backgroundColor: Colors.grey[900],
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBody() {
+    return CustomScrollView(
+      slivers: [
+        _buildSliverAppBar(),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return ListTile(
+                title: Text('Item #$index', style: const TextStyle(color: Colors.white)),
+                tileColor: index % 2 == 0 ? Colors.blueAccent : Colors.lightBlue,
+              );
+            },
+            childCount: 20, // Number of list items
+          ),
+        ),
+      ],
+    );
+  }
+
+  SliverAppBar _buildSliverAppBar() {
+    return SliverAppBar(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      pinned: true,
+      elevation: 0,
+      title: Text(
+        widget.title,
+        style: const TextStyle(fontSize: 20),
       ),
-      body: const Center(
-        child: Text(
-          'Prova',
+      leading: IconButton(
+        icon: const Icon(Icons.square, color: Colors.white),
+        onPressed: () {
+          // Action for the left icon
+        },
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.square, color: Colors.white),
+          onPressed: () {
+            // Action for the right icon
+          },
+        ),
+      ],
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              Colors.transparent,
+            ],
+          ),
         ),
       ),
-      backgroundColor: Colors.blueAccent,
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: CrystalNavigationBar(
-          enableFloatingNavBar: true,
-          enablePaddingAnimation: true,
-          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
-          unselectedItemColor: Colors.white70,
-          backgroundColor: Colors.black.withOpacity(0.1),
-          splashColor: Colors.transparent,
-          indicatorColor: Colors.transparent,
-          onTap: _handleIndexChanged,
-          items: [
-            // Home
-            CrystalNavigationBarItem(
-              icon: Icons.square,
-              unselectedIcon: Icons.square,
-              selectedColor: Colors.white,
-            ),
-            // Search
-            CrystalNavigationBarItem(
-              icon: Icons.square,
-              unselectedIcon: Icons.square,
-              selectedColor: Colors.white,
-            ),
-            // Add
-            CrystalNavigationBarItem(
-              icon: Icons.square,
-              unselectedIcon: Icons.square,
-              selectedColor: Colors.white,
-            ),
-            // Library
-            CrystalNavigationBarItem(
-              icon: Icons.square,
-              unselectedIcon: Icons.square,
-              selectedColor: Colors.yellow,
-            ),
-            // Profile
-            CrystalNavigationBarItem(
-              icon: Icons.square,
-              unselectedIcon: Icons.square,
-              selectedColor: Colors.white,
-            ),
-          ],
-        ),
-      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return CrystalNavigationBar(
+      enableFloatingNavBar: true,
+      enablePaddingAnimation: true,
+      currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+      unselectedItemColor: Colors.white70,
+      backgroundColor: Colors.black.withOpacity(0.1),
+      splashColor: Colors.transparent,
+      indicatorColor: Colors.transparent,
+      onTap: _handleIndexChanged,
+      items: _buildNavigationBarItems(),
+    );
+  }
+
+  List<CrystalNavigationBarItem> _buildNavigationBarItems() {
+    return [
+      _buildNavigationBarItem(Icons.square, Colors.white),
+      _buildNavigationBarItem(Icons.square, Colors.white),
+      _buildNavigationBarItem(Icons.square, Colors.white),
+      _buildNavigationBarItem(Icons.square, Colors.yellow),
+      _buildNavigationBarItem(Icons.square, Colors.white),
+    ];
+  }
+
+  CrystalNavigationBarItem _buildNavigationBarItem(IconData icon, Color selectedColor) {
+    return CrystalNavigationBarItem(
+      icon: icon,
+      unselectedIcon: icon,
+      selectedColor: selectedColor,
     );
   }
 }
