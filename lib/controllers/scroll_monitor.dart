fdb5ed259130c3203c12338ff179cef39548c53a
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-11-10 10:48:23 +0100

    optimized scroll_monitor.dart and navigation_bar.dart

--- lib/controllers/scroll_monitor.dart
+++ lib/controllers/scroll_monitor.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:vynt/constants.dart' as constants;
 
 class ScrollMonitor extends ChangeNotifier {
   final ScrollController _scrollController = ScrollController();
-  double navBarOffset = 0.0;
+  bool isScrollingDown = false;
+  static const double scrollThreshold = 75.0;
   double _lastOffset = 0.0;
 
   ScrollMonitor() {
@@ -15,31 +15,20 @@ class ScrollMonitor extends ChangeNotifier {
 
   void _scrollListener() {
     double currentOffset = _scrollController.offset;
-    double deltaOffset = currentOffset - _lastOffset;
-    _lastOffset = currentOffset;
-
-    navBarOffset -= deltaOffset;
-    navBarOffset = navBarOffset.clamp(-constants.minNavBarHeight, 0.0);
-    if (_scrollController.position.userScrollDirection != ScrollDirection.idle) {
-      snapNavBar();
+    if ((currentOffset - _lastOffset).abs() > scrollThreshold) {
+      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        if (!isScrollingDown) {
+          isScrollingDown = true;
+          notifyListeners();
+        }
+      } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+        if (isScrollingDown) {
+          isScrollingDown = false;
+          notifyListeners();
+        }
+      }
+      _lastOffset = currentOffset;
     }
-    notifyListeners();
-  }
-
-  void updateNavBarOffset(double delta) {
-    navBarOffset += delta;
-    navBarOffset = navBarOffset.clamp(-constants.minNavBarHeight, 0.0);
-    notifyListeners();
-  }
-
-  void snapNavBar() {
-    double snapThreshold = -constants.minNavBarHeight / 4;
-    if (navBarOffset > snapThreshold) {
-      navBarOffset = 0.0;
-    } else {
-      navBarOffset = -constants.minNavBarHeight;
-    }
-    notifyListeners();
   }
 
   @override
