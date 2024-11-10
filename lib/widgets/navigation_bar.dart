2f041d66457094791aed4e6cce1d9973752fabc2
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-11-10 02:17:01 +0100

    Added snapping to the top and bottom of navbar

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
-
 import 'package:vynt/controllers/scroll_monitor.dart';
 
 class CustomNavigationBar extends StatelessWidget {
@@ -23,64 +20,66 @@ class CustomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        pages[selectedIndex],
-        Consumer<ScrollMonitor>(
-          builder: (context, scrollMonitor, child) {
-            return AnimatedPositioned(
-              duration: const Duration(milliseconds: 250),
-              left: 0,
-              right: 0,
-              bottom: scrollMonitor.isScrollingDown
-                  ? -kBottomNavigationBarHeight - 75
-                  : 0,
-              child: CrystalNavigationBar(
-                enableFloatingNavBar: true,
-                enablePaddingAnimation: true,
-                currentIndex: selectedIndex,
-                unselectedItemColor: Colors.white70,
-                backgroundColor: Colors.black.withOpacity(0.1),
-                splashColor: Colors.transparent,
-                indicatorColor: Colors.transparent,
-                onTap: onItemTapped,
-                items: [
-                  CrystalNavigationBarItem(
-                    icon: CupertinoIcons.house_fill,
-                    unselectedIcon: CupertinoIcons.house,
-                    selectedColor: Colors.black,
-                    unselectedColor: Colors.grey,
-                  ),
-                  CrystalNavigationBarItem(
-                    icon: CupertinoIcons.search,
-                    unselectedIcon: CupertinoIcons.search,
-                    selectedColor: Colors.black,
-                    unselectedColor: Colors.grey,
-                  ),
-                  CrystalNavigationBarItem(
-                    icon: CupertinoIcons.add,
-                    unselectedIcon: CupertinoIcons.add,
-                    selectedColor: Colors.black,
-                    unselectedColor: Colors.grey,
-                  ),
-                  CrystalNavigationBarItem(
-                    icon: CupertinoIcons.music_albums_fill,
-                    unselectedIcon: CupertinoIcons.music_albums,
-                    selectedColor: Colors.black,
-                    unselectedColor: Colors.grey,
-                  ),
-                  CrystalNavigationBarItem(
-                    icon: CupertinoIcons.person_fill,
-                    unselectedIcon: CupertinoIcons.person,
-                    selectedColor: Colors.black,
-                    unselectedColor: Colors.grey,
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ],
+    return GestureDetector(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: pages[selectedIndex],
+          ),
+          Consumer<ScrollMonitor>(
+            builder: (context, scrollMonitor, child) {
+              return AnimatedPositioned(
+                duration: const Duration(milliseconds: 250),
+                left: 0,
+                right: 0,
+                bottom: scrollMonitor.navBarOffset,
+                child: CrystalNavigationBar(
+                  enableFloatingNavBar: true,
+                  enablePaddingAnimation: true,
+                  currentIndex: selectedIndex,
+                  unselectedItemColor: Colors.white70,
+                  backgroundColor: Colors.black.withOpacity(0.1),
+                  splashColor: Colors.transparent,
+                  indicatorColor: Colors.transparent,
+                  onTap: onItemTapped,
+                  items: [
+                    CrystalNavigationBarItem(
+                      icon: CupertinoIcons.house_fill,
+                      unselectedIcon: CupertinoIcons.house,
+                      selectedColor: Colors.black,
+                      unselectedColor: Colors.grey,
+                    ),
+                    CrystalNavigationBarItem(
+                      icon: CupertinoIcons.search,
+                      unselectedIcon: CupertinoIcons.search,
+                      selectedColor: Colors.black,
+                      unselectedColor: Colors.grey,
+                    ),
+                    CrystalNavigationBarItem(
+                      icon: CupertinoIcons.add,
+                      unselectedIcon: CupertinoIcons.add,
+                      selectedColor: Colors.black,
+                      unselectedColor: Colors.grey,
+                    ),
+                    CrystalNavigationBarItem(
+                      icon: CupertinoIcons.music_albums_fill,
+                      unselectedIcon: CupertinoIcons.music_albums,
+                      selectedColor: Colors.black,
+                      unselectedColor: Colors.grey,
+                    ),
+                    CrystalNavigationBarItem(
+                      icon: CupertinoIcons.person_fill,
+                      unselectedIcon: CupertinoIcons.person,
+                      selectedColor: Colors.black,
+                      unselectedColor: Colors.grey,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
