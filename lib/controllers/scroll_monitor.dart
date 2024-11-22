234de1c06e93fe8eb09ff23705fa24b1652612fd
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-11-22 17:41:10 +0100

    Optimized scroll_monitor.dart (again)

--- lib/controllers/scroll_monitor.dart
+++ lib/controllers/scroll_monitor.dart
@@ -3,9 +3,9 @@ import 'package:flutter/rendering.dart';
 
 class ScrollMonitor extends ChangeNotifier {
   final ScrollController _scrollController = ScrollController();
-  bool isScrollingDown = false;
-  static const double scrollThreshold = 100.0;
+  bool _isScrollingDown = false;
   double _lastOffset = 0.0;
+  static const double scrollThreshold = 50.0;
 
   ScrollMonitor() {
     _scrollController.addListener(_scrollListener);
@@ -13,23 +13,31 @@ class ScrollMonitor extends ChangeNotifier {
 
   ScrollController get scrollController => _scrollController;
 
-  void _scrollListener() {
-    double currentOffset = _scrollController.offset;
-    double offsetDifference = (currentOffset - _lastOffset).abs();
+  bool get isScrollingDown => _isScrollingDown;
 
-    if (offsetDifference > scrollThreshold) {
-      ScrollDirection direction = _scrollController.position.userScrollDirection;
-
-      if (direction == ScrollDirection.reverse && !isScrollingDown) {
-        isScrollingDown = true;
-        _lastOffset = currentOffset;
-        notifyListeners();
-      } else if (direction == ScrollDirection.forward && isScrollingDown) {
-        isScrollingDown = false;
-        _lastOffset = currentOffset;
-        notifyListeners();
-      }
+  set isScrollingDown(bool value) {
+    if (_isScrollingDown != value) {
+      _isScrollingDown = value;
+      notifyListeners();
     }
+  }
+
+  void _scrollListener() {
+    final double currentOffset = _scrollController.offset;
+
+    if ((currentOffset - _lastOffset).abs() < scrollThreshold) {
+      return; // Ignore small scroll changes
+    }
+
+    final ScrollDirection direction = _scrollController.position.userScrollDirection;
+
+    if (direction == ScrollDirection.reverse && !_isScrollingDown) {
+      isScrollingDown = true;
+    } else if (direction == ScrollDirection.forward && _isScrollingDown) {
+      isScrollingDown = false;
+    }
+
+    _lastOffset = currentOffset;
   }
 
   @override
