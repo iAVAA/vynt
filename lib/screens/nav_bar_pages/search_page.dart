8cdae414bdd57437ef6982cb5e9543fbab097346
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-12-13 15:46:20 +0100

    Added mockup of card swiping method

--- lib/screens/nav_bar_pages/search_page.dart
+++ lib/screens/nav_bar_pages/search_page.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'package:marquee/marquee.dart';
-
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:provider/provider.dart';
-
 import 'package:vynt/controllers/scroll_monitor.dart';
 import 'package:vynt/constants/constants.dart' as constants;
 
-class Search extends StatelessWidget {
+import '../../widgets/nav_bar_pages_widgets/example_candidate_model.dart';
+import '../../widgets/nav_bar_pages_widgets/example_card.dart';
+
+class Search extends StatefulWidget {
   const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final CardSwiperController controller = CardSwiperController();
+
+  final cards = candidates.map(ExampleCard.new).toList();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,40 +58,58 @@ class Search extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: SizedBox(
-              height: 110,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-                itemCount: 10,
-                itemBuilder: (context, index) => _buildEventItem(),
-              ),
-            ),
-          ),
-          SliverGrid(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: constants.secondaryBgColor,
-                    borderRadius: BorderRadius.circular(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 400.0,
+                  child: CardSwiper(
+                    controller: controller,
+                    cardsCount: cards.length,
+                    onSwipe: _onSwipe,
+                    onUndo: _onUndo,
+                    numberOfCardsDisplayed: 3,
+                    padding: const EdgeInsets.all(24.0),
+                    cardBuilder: (
+                      context,
+                      index,
+                      horizontalThresholdPercentage,
+                      verticalThresholdPercentage,
+                    ) => cards[index],
                   ),
-                  child: Center(
-                    child: Icon(
-                      Icons.image,
-                      color: Colors.grey[700],
-                      size: 40,
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: controller.undo,
+                        child: const Icon(Icons.rotate_left),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () =>
+                            controller.swipe(CardSwiperDirection.left),
+                        child: const Icon(Icons.keyboard_arrow_left),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () =>
+                            controller.swipe(CardSwiperDirection.right),
+                        child: const Icon(Icons.keyboard_arrow_right),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () =>
+                            controller.swipe(CardSwiperDirection.top),
+                        child: const Icon(Icons.keyboard_arrow_up),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () =>
+                            controller.swipe(CardSwiperDirection.bottom),
+                        child: const Icon(Icons.keyboard_arrow_down),
+                      ),
+                    ],
                   ),
-                );
-              },
-              childCount: 30,
+                ),
+              ],
             ),
           ),
         ],
@@ -84,54 +117,25 @@ class Search extends StatelessWidget {
     );
   }
 
-  Widget _buildEventItem() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey[700],
-            ),
-            child: Icon(
-              Icons.person,
-              color: Colors.grey[500],
-              size: 25,
-            ),
-          ),
-          const SizedBox(height: 6),
-          SizedBox(
-            width: 70,
-            height: 18,
-            child: Marquee(
-              text: 'Event ${DateTime.now()}',
-              style: TextStyle(color: constants.primaryTextColor, fontSize: 11),
-              scrollAxis: Axis.horizontal,
-              blankSpace: 20.0,
-              velocity: 30.0,
-              startPadding: 10.0,
-              pauseAfterRound: const Duration(seconds: 1),
-            ),
-          ),
-          const SizedBox(height: 4),
-          SizedBox(
-            width: 70,
-            height: 16,
-            child: Marquee(
-              text: 'Artist Name',
-              style: TextStyle(color: constants.secondaryTextColor, fontSize: 9),
-              scrollAxis: Axis.horizontal,
-              blankSpace: 20.0,
-              velocity: 30.0,
-              startPadding: 10.0,
-              pauseAfterRound: const Duration(seconds: 1),
-            ),
-          ),
-        ],
-      ),
+  bool _onSwipe(
+    int previousIndex,
+    int? currentIndex,
+    CardSwiperDirection direction,
+  ) {
+    debugPrint(
+      'The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top',
     );
+    return true;
+  }
+
+  bool _onUndo(
+    int? previousIndex,
+    int currentIndex,
+    CardSwiperDirection direction,
+  ) {
+    debugPrint(
+      'The card $currentIndex was undod from the ${direction.name}',
+    );
+    return true;
   }
 }
