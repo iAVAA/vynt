479aa2c1355679755df74d5ff8bc99ab3ab91084
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-12-15 17:00:57 +0100

    Fixed the swiping direction and removed sliding view, so now it's good

--- lib/screens/nav_bar_pages/search_page.dart
+++ lib/screens/nav_bar_pages/search_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-import 'package:vynt/controllers/scroll_monitor.dart';
 import 'package:vynt/constants/constants.dart' as constants;
 
 import '../../widgets/nav_bar_pages_widgets/example_candidate_model.dart';
@@ -29,93 +27,88 @@ class _DiscoverState extends State<Discover> {
 
   @override
   Widget build(BuildContext context) {
-    final scrollMonitor = Provider.of<ScrollMonitor>(context);
-
     return Scaffold(
       backgroundColor: constants.bgColor,
-      body: CustomScrollView(
-        key: const PageStorageKey('discover'),
-        controller: scrollMonitor.scrollController,
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 70.0),
-                  child: Text(
-                    'Library',
-                    style: GoogleFonts.poppins(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70.0),
+                child: Text(
+                  'Discover',
+                  style: GoogleFonts.poppins(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: CupertinoSearchTextField(
+                  backgroundColor: constants.secondaryBgColor,
+                  borderRadius: BorderRadius.circular(10),
+                  placeholder: 'Search',
+                  placeholderStyle: TextStyle(color: constants.secondaryTextColor),
+                  itemColor: Colors.grey,
+                  style: TextStyle(color: constants.primaryTextColor),
+                ),
+              ),
+              SizedBox(
+                height: 500.0,
+                child: CardSwiper(
+                  controller: controller,
+                  cardsCount: cards.length,
+                  onSwipe: _onSwipe,
+                  onUndo: _onUndo,
+                  numberOfCardsDisplayed: 2,
+                  allowedSwipeDirection: const AllowedSwipeDirection.only(
+                    right: true,
+                    left: true,
+                    up: true,
+                  ),
+                  cardBuilder: (
+                    context,
+                    index,
+                    horizontalThresholdPercentage,
+                    verticalThresholdPercentage,
+                  ) =>
+                      cards[index],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: controller.undo,
+                      child: const Icon(Icons.rotate_left),
                     ),
-                  ),
+                    FloatingActionButton(
+                      onPressed: () => controller.swipe(CardSwiperDirection.left),
+                      child: const Icon(Icons.keyboard_arrow_left),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () => controller.swipe(CardSwiperDirection.right),
+                      child: const Icon(Icons.keyboard_arrow_right),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () => controller.swipe(CardSwiperDirection.top),
+                      child: const Icon(Icons.keyboard_arrow_up),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () => controller.swipe(CardSwiperDirection.bottom),
+                      child: const Icon(Icons.keyboard_arrow_down),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                  child: CupertinoSearchTextField(
-                    backgroundColor: constants.secondaryBgColor,
-                    borderRadius: BorderRadius.circular(10),
-                    placeholder: 'Search',
-                    placeholderStyle:
-                        TextStyle(color: constants.secondaryTextColor),
-                    itemColor: Colors.grey,
-                    style: TextStyle(color: constants.primaryTextColor),
-                  ),
-                ),
-                SizedBox(
-                  height: 500.0,
-                  child: CardSwiper(
-                    controller: controller,
-                    cardsCount: cards.length,
-                    onSwipe: _onSwipe,
-                    onUndo: _onUndo,
-                    numberOfCardsDisplayed: 2,
-                    cardBuilder: (
-                      context,
-                      index,
-                      horizontalThresholdPercentage,
-                      verticalThresholdPercentage,
-                    ) =>
-                        cards[index],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      FloatingActionButton(
-                        onPressed: controller.undo,
-                        child: const Icon(Icons.rotate_left),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () =>
-                            controller.swipe(CardSwiperDirection.left),
-                        child: const Icon(Icons.keyboard_arrow_left),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () =>
-                            controller.swipe(CardSwiperDirection.right),
-                        child: const Icon(Icons.keyboard_arrow_right),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () =>
-                            controller.swipe(CardSwiperDirection.top),
-                        child: const Icon(Icons.keyboard_arrow_up),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () =>
-                            controller.swipe(CardSwiperDirection.bottom),
-                        child: const Icon(Icons.keyboard_arrow_down),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
