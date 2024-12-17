af56562fe4cf98d4c16d634a669d25342932d525
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-12-17 23:52:29 +0100

    Added the colors for the new design

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -7,29 +7,16 @@ Color? secondaryBgColor = Colors.grey.shade800;
 Color primaryTextColor = Colors.white;
 Color secondaryTextColor = Colors.grey;
 
-/**
- * Color Palette
- *
- * !Primary Color (Purple from the logo): #9C27B0
- * This vibrant shade of purple harmonizes well with the vinyl record motif and works great as a primary accent.
- *
- * !Secondary Color (Gradient pink in the logo): #E91E63
- * Use this for highlights, buttons, or action elements to enhance contrast.
- *
- * !Tertiary Color (Complementary light blue): #03A9F4
- * A subtle contrast to purple, great for icons, small details, or notifications.
- *
- * !Background Accent Colors:
- * Dark mode: #303030 (subtle background layers)
- * Light mode: #F5F5F5 (for card backgrounds or dividers)
- *
- * !Neutral Colors (Text and Icons):
- * Dark mode text: #E0E0E0 (primary) and #BDBDBD (secondary)
- * Light mode text: #212121 (primary) and #757575 (secondary)
- * Highlight/Accent (Bright Vinyl Glow): #FFD700 (gold) for a glow effect or special highlights, referencing vinyl grooves.
- *
- * !Error/Warning Colors:
- * Error: #F44336
- * Warning: #FF9800
- * Success/Confirmation Colors: #4CAF50
- */
+Color warningColor = Color(0xFFFF9800);
+Color errorColor = Color(0xFFF44336);
+Color successColor = Color(0xFF13AD5D);
+
+// Dark Mode Colors
+Color darkPrimaryColor = Color(0xFFF3E2FB);
+Color darkSecondaryColor = Color(0xFFDC40A4);
+Color darkBackgroundColor = Color(0xFF320943);
+
+// Light Mode Colors
+Color lightPrimaryColor = Color(0xFF320943);
+Color lightSecondaryColor = Color(0xFFDC40A4);
+Color lightBackgroundColor = Color(0xFFF3E2FB);
