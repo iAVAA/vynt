e3c9c1a53811284f39420e0dc9759a396b6489f5
Salvatore Iavarone <lorenzoiava11@gmail.com>
2024-12-25 00:38:12 +0100

    added auth_provider.dart for logins

--- lib/screens/login_pages/main_login_page.dart
+++ lib/screens/login_pages/main_login_page.dart
@@ -19,6 +19,8 @@ import 'package:vynt/screens/main_page.dart';
 import 'package:vynt/widgets/login_pages_widgets/onboarding_widgets.dart';
 import 'package:vynt/controllers/theme_controller.dart';
 
+import 'package:vynt/providers/auth_provider.dart';
+
 import 'login_page.dart';
 
 class MainLoginPage extends StatefulWidget {
@@ -29,67 +31,6 @@ class MainLoginPage extends StatefulWidget {
 }
 
 class _MainLoginPageState extends State<MainLoginPage> {
-  Future<void> signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-      final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
-
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken,
-      );
-
-      UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithCredential(credential);
-      await saveUserData(userCredential.user);
-
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const Home()),
-        );
-      }
-    } catch (e) {
-      print('Error signing in with Google: $e');
-    }
-  }
-
-  String generateNonce([int length = 32]) {
-    final charset =
-        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-    final random = Random.secure();
-    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-        .join();
-  }
-
-  String sha256ofString(String input) {
-    final bytes = utf8.encode(input);
-    final digest = sha256.convert(bytes);
-    return digest.toString();
-  }
-
-  Future<UserCredential> signInWithApple() async {
-    final rawNonce = generateNonce();
-    final nonce = sha256ofString(rawNonce);
-
-    final appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-      nonce: nonce,
-    );
-
-    final oauthCredential = OAuthProvider("apple.com").credential(
-      idToken: appleCredential.identityToken,
-      rawNonce: rawNonce,
-    );
-
-    return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -188,15 +129,28 @@ class _ActionButtons extends StatelessWidget {
           const SizedBox(height: 20),
           LineSeparatorWithText(
             text: 'OR',
-            lineColor: Theme.of(context).textTheme.bodyLarge?.color ?? Colors.grey,
-            textColor: Theme.of(context).textTheme.bodyLarge?.color ?? Colors.grey,
+            lineColor:
+                Theme.of(context).textTheme.bodyLarge?.color ?? Colors.grey,
+            textColor:
+                Theme.of(context).textTheme.bodyLarge?.color ?? Colors.grey,
           ),
           const SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SquareTile(
-                onTap: () => state.signInWithGoogle(),
+                onTap: () async {
+                  if (await GoogleProvider().signIn()) {
+                    if (context.mounted) {
+                      Navigator.pushReplacement(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => const Home(),
+                        ),
+                      );
+                    }
+                  }
+                },
                 imagePath: 'assets/icons/icon_google.svg',
                 imageHeight: 50,
               ),
